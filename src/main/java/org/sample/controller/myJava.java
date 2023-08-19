028f298fb68e998d6f8cb7041307a914bb54fe0e
amit.kumar.mishra <kumar.amit.mishra@gmail.com>
2023-08-19 17:26:21 +1000

    first commit

---
+++ src/main/java/org/sample/controller/myJava.java
@@ -0,0 +1,12 @@
+package org.sample.controller;
+
+public class myJava {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public myJava(String name){
+        this.name=name;
+    }
+}
